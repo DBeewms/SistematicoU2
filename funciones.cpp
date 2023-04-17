48267af63c9e8860957b99a65a929d54ae296247
DBeewms <diego.mora16063@est.uca.edu.ni>
2023-04-17 08:20:27 -0600

    Se agrega N2 del ejercicio

--- funciones.cpp
+++ funciones.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main () {
-    int cantidad, num;
+
+
+void numerosMayor(){
+    int mayor;
+    int cantidad;
+    int num;
     int mayor = 0;
     int suma = 0;
     
-    cout<<"Cuantos numeros: ";
-    cin>> cantidad;
+    cout << "Cuantos numeros vas a analizar: " << endl;
+    cin >> cantidad;
         for (int i = 1; i <= cantidad; i++){
-        cout<<"Ingrese el numero: ";
+        cout<<"Ingrese el numero: " << endl;
         cin>>num;
 
         if(1 == i){ 
@@ -23,9 +27,31 @@ int main () {
     
         suma += num;
         }
-        cout<<"El numero mayor es: "<<mayor<<endl;
+        cout << "El numero mayor es: "<< mayor << endl;
+return;
 
-        return 0;
+}
+
+
+void tablaMultiplicar(){
+    int multiplicar;
+    int numero;
+    int i=1;
+    cout<<"Ingresa el numero y te dare su tabla de multiplicar : ";
+
+    cin>>numero;
+
+    cout<<"La tabla del "<<numero<<" es :"<<endl;
+
+    do{
+    multiplicar = numero * i;
+    cout<< numero <<" x "<< i <<" = "<< multiplicar <<endl;
+    i++;
     }
+    while(i<=10);
+
+    return;
+}
+    
 
 
